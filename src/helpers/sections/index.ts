f320e96a7ef719e63a69f40f108c6d0238ebb3b6
kmacoders <rio@rios-MacBook-Pro.local>
2021-09-18 12:02:03 +0700

    refactor: package info

--- src/helpers/sections/index.ts
+++ src/helpers/sections/index.ts
@@ -8,23 +8,3 @@ import { qs } from '../dom/dom';
 export const onSectionSelected = (className: string, cb: () => void): void => {
   qs(className) && cb();
 };
-
-/**
- * Debounce function
- * @param {Function} func : Function want to delay
- * @param {number} waitFor : Time to delay
- * @returns {Function}
- */
-
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const debounce = <F extends (...args: any) => any>(func: F, waitFor: number) => {
-  let timeout = 0;
-
-  const debounced = (...args: any): void => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), waitFor);
-  };
-
-  return debounced as (...args: Parameters<F>) => ReturnType<F>;
-};
